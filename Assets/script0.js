d96c5caa562ae353cf9cd3e1a187a4c041a56af7
Sal <salvador.banuelos.424@gmail.com>
2022-03-26 18:38:19 -0700

    add second js

---
+++ Assets/script0.js
@@ -0,0 +1,6 @@
+let btn = document.createElement("button8");
+btn.innerHTML = "Save";
+btn.addEventListener("click", function () {
+  alert("Button is clicked");
+});
+document.body.appendChild(btn);
